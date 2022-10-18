846b65723b03947ed14b48979606f9a8c917cb33
sokolovstanislav <ignateva-1946@mail.ru>
2022-10-18 17:29:41 +0300

    Good things

--- src/main/java/Redirect.java
+++ src/main/java/Redirect.java
@@ -11,17 +11,34 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/")
 public class Redirect extends HttpServlet {
+    private final String DOMEN_1 = "falconfamily.shop";
+    private final String DOMEN_2 = "tapasandpintxos.ru";
+    private final String PORT = "8448";
     @Override
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         httpServletRequest.setCharacterEncoding("UTF-8");
         httpServletResponse.setContentType("text/html");
         String request = String.valueOf(httpServletRequest.getRequestURL());
-        if (request.equals("http://www.falconfamily.shop/")) {
-            String path = httpServletRequest.getContextPath() + "/MebelCity";
-            httpServletResponse.sendRedirect(path);
-//        } else if (request.equals("http://www.tapasandpintxos.ru/")) {
+        if (request.equals("http://www." + DOMEN_1 + "/") ||
+                request.equals("http://" + DOMEN_1 + "/") ||
+                request.equals("https://www." + DOMEN_1 + "/") ||
+                request.equals("https://" + DOMEN_1 + "/") ||
+                request.equals("https://www." + DOMEN_1 + ":" + PORT + "/") ||
+                request.equals("https://" + DOMEN_1 + ":" + PORT + "/")) {
+//            String path = httpServletRequest.getContextPath() + "/MebelCity";
+            httpServletResponse.sendRedirect("https://www." + DOMEN_1 + "/MebelCity");
+        } else if (request.equals("http://www." + DOMEN_2 + "/") ||
+                request.equals("http://" + DOMEN_2 + "/") ||
+                request.equals("https://www." + DOMEN_2 + "/") ||
+                request.equals("https://" + DOMEN_2 + "/") ||
+                request.equals("https://www." + DOMEN_2 + ":" + PORT + "/") ||
+                request.equals("https://" + DOMEN_2 + ":" + PORT + "/")) {
 //            String path = httpServletRequest.getContextPath() + "/docs";
 //            httpServletResponse.sendRedirect(path);
+            httpServletResponse.sendRedirect("https://www." + DOMEN_2 + "/docs");
+        } else {
+            String path = httpServletRequest.getContextPath() + "/test";
+            httpServletResponse.sendRedirect(path);
         }
     }
 
